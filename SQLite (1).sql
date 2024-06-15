465e52c148b91e4e590f2a36dffbfbfdb8fcd2d6
Kingston21112002 <meshak7010732228@gmail.com>
2024-06-15 20:02:01 +0530

    Add files via upload

---
+++ SQLite (1).sql
@@ -0,0 +1,33 @@
+CREATE TABLE employees (
+    employee_id INT PRIMARY KEY,
+    first_name VARCHAR(50),
+    last_name VARCHAR(50),
+    department_id INT,
+    hire_date DATE,
+    FOREIGN KEY (department_id) REFERENCES departments(department_id)
+);
+CREATE TABLE departments (
+    department_id INT PRIMARY KEY,
+    department_name VARCHAR(100)
+);
+CREATE TABLE salaries (
+    employee_id INT,
+    salary DECIMAL(10, 2),
+    from_date DATE,
+    to_date DATE,
+    FOREIGN KEY (employee_id) REFERENCES employees(employee_id)
+);
+SELECT *
+FROM employees
+WHERE hire_date >= DATE_SUB(CURDATE(), INTERVAL 1 YEAR);
+SELECT d.department_name, SUM(s.salary) AS total_salary_expenditure
+FROM departments d
+JOIN employees e ON d.department_id = e.department_id
+JOIN salaries s ON e.employee_id = s.employee_id
+GROUP BY d.department_name;
+SELECT e.first_name, e.last_name, d.department_name, s.salary
+FROM employees e
+JOIN departments d ON e.department_id = d.department_id
+JOIN salaries s ON e.employee_id = s.employee_id
+ORDER BY s.salary DESC
+LIMIT 5;
